94ed7d0da16d4283bd39009263a4b1d5e5d794cb
Jan Pawellek <jan@janpawellek.de>
2015-06-11 00:10:10 +0200

    Refactored JS code according to JSLint

--- www/assets/js/account.js
+++ www/assets/js/account.js
@@ -1,70 +1,72 @@
-"use strict";
-// initialize Hoodie
-// var hoodie  = new Hoodie();
+/*global $,moment,Hoodie*/
+(function () {
+    "use strict";
+    // initialize Hoodie
+    var hoodie  = new Hoodie();
 
-// HELPER FUNCTIONS -----------------------------
-function setLoggedIn(state) {
-  if (state) {
-    // set page layout to logged in state
-    $('.onLoginClearVal').val('');
-    $('.onLoginHide').addClass('hidden');
-    $('.onLoginShow').removeClass('hidden');
+    // HELPER FUNCTIONS -----------------------------
+    function setLoggedIn(state) {
+        if (state) {
+            // set page layout to logged in state
+            $('.onLoginClearVal').val('');
+            $('.onLoginHide').addClass('hidden');
+            $('.onLoginShow').removeClass('hidden');
 
-    // hide login dialog and show user name
-    $('#loginModal').modal('hide');
-    $('.hoodieUsername').html(hoodie.account.username);
-  }
-  else {
-    // set page layout to logged out state
-    $('.onLogoffShow').removeClass('hidden');
-    $('.onLogoffHide').addClass('hidden');
+            // hide login dialog and show user name
+            $('#loginModal').modal('hide');
+            $('.hoodieUsername').html(hoodie.account.username);
+        } else {
+            // set page layout to logged out state
+            $('.onLogoffShow').removeClass('hidden');
+            $('.onLogoffHide').addClass('hidden');
 
-    // important: clear everything in the DOM from the previously logged in user
-    $('.onLogoffClearContent').html('');
-    $('.onLogoffClearVal').val('');
+            // important: clear everything in the DOM from the previously logged in user
+            $('.onLogoffClearContent').html('');
+            $('.onLogoffClearVal').val('');
 
-    // insert today's date as default
-    $('.insertToday').val(moment().format('DD.MM.YYYY'));
-  };
-}
-
-// EVENTS ---------------------------------------
-$('#loginForm').submit(function(event) {
-  event.preventDefault();
-  var username = $('#loginName').val();
-  var password = $('#loginPassword').val();
-
-  hoodie.account.signIn(username, password)
-  .done(
-    function() {
-      // login successful
-      setLoggedIn(true);
+            // insert today's date as default
+            $('.insertToday').val(moment().format('DD.MM.YYYY'));
+        }
     }
-  ).fail(
-    function() {
-      // login failed
-      $('#loginFailed').removeClass('hidden');
-      setLoggedIn(false);
+
+    // EVENTS ---------------------------------------
+    $('#loginForm').submit(function (event) {
+        event.preventDefault();
+        var username = $('#loginName').val(),
+            password = $('#loginPassword').val();
+
+        hoodie.account.signIn(username, password)
+            .done(
+                function () {
+                    // login successful
+                    setLoggedIn(true);
+                }
+            ).fail(
+                function () {
+                    // login failed
+                    $('#loginFailed').removeClass('hidden');
+                    setLoggedIn(false);
+                }
+            );
+    });
+
+    $('#logoutButton').click(function () {
+        hoodie.account.signOut();
+        setLoggedIn(false);
+    });
+
+    hoodie.account.on('error:unauthenticated signout', function () {
+        setLoggedIn(false);
+    });
+
+    hoodie.account.on('signin signup', function () {
+        setLoggedIn(true);
+    });
+
+    // ON PAGE LOAD ---------------------------------
+    if (hoodie.account.username === undefined) {
+        setLoggedIn(false);
+    } else {
+        setLoggedIn(true);
     }
-  );
-});
-
-$('#logoutButton').click(function(){
-  hoodie.account.signOut();
-  setLoggedIn(false);
-});
-
-hoodie.account.on('error:unauthenticated signout', function() {
-  setLoggedIn(false);
-});
-
-hoodie.account.on('signin signup', function(){
-  setLoggedIn(true);
-});
-
-// ON PAGE LOAD ---------------------------------
-if (hoodie.account.username == undefined) {
-  setLoggedIn(false);
-} else {
-  setLoggedIn(true);
-};
+}());
--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,31 +1,236 @@
-"use strict";
+/*global $,moment,Hoodie*/
+(function () {
+    "use strict";
+    var hoodie,
+        App,
+        Transactions;
 
-// initialize Hoodie
-var hoodie = new Hoodie();
+    // initialize Hoodie
+    hoodie = new Hoodie();
 
-// helper function to escape HTML
-function escapeHtml(string) {
-  var entityMap = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': '&quot;',
-    "'": '&#39;',
-    "/": '&#x2F;'
-  };
-  return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s];
-  });
-}
+    // helper function to escape HTML
+    function escapeHtml(string) {
+        var entityMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+        };
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+            return entityMap[s];
+        });
+    }
 
-// execute when DOM is ready
-$(function() {
+    // TRANSACTIONS ---------------------------------
+    // Generic class for Transactions
+    Transactions = function ($element) {
+        var collection = [],
+            $el = $element,
+            sum = 0.0;
 
-  // enable autoNumeric to help entering currency data
-  $('.autonumeric').autoNumeric('init', {aSep: '.', aDec: ',', aSign: ' €', pSign: 's'});
+        function getTransactionItemIndexById(id) {
+            var i;
+            for (i = 0; i < collection.length; i += 1) {
+                if (collection[i].id === id) {
+                    return i;
+                }
+            }
+            return null;
+        }
 
-  // insert today's date as default
-  $('.insertToday').val(moment().format('DD.MM.YYYY'));
+        function paint() {
+            var i,
+                curamountid,
+                curamount,
+                roundedsum;
+            sum = 0.0;
+            $el.html('');
+            collection.sort(function (a, b) {
+                return (a.date > b.date) ? 1 : -1;
+            });
+            for (i = 0; i < collection.length; i += 1) {
+                curamountid = $el.attr('id') + '-amount-' + collection[i].id;
+                $el.append(
+                    '<tr data-id="' + collection[i].id + '">' +
+                        '<td>' + escapeHtml(moment(new Date(collection[i].date)).format('DD.MM.YYYY')) + '</td>' +
+                        '<td>' + escapeHtml(collection[i].subject) + '</td>' +
+                        '<td class="autonumeric" id="' + curamountid + '">' + escapeHtml(collection[i].amount) + '</td>' +
+                        '</tr>'
+                );
+                try {
+                    $('#' + curamountid).autoNumeric('init', {aSep: '.', aDec: ',', aSign: ' €', pSign: 's'});
 
-  var spendApp = new App('spend');
-});
+                    // get amount as number to compute sum
+                    curamount = $('#' + curamountid).autoNumeric('get');
+                    sum += parseFloat(curamount);
+                } catch (e) {
+                    // TODO validation failed, collection[i].amount is not numeric
+                    window.console.log('non-numerical amount');
+                }
+            }
+
+            // add final sum row to table
+            $el.append(
+                '<tr>' +
+                    '<td></td>' +
+                    '<td><b>Summa summarum:</b></td>' +
+                    '<td style="font-weight: bold;" class="autonumeric" id="' + $el.attr('id') + '-sum-row' + '"><b>' + sum + '</b></td>' +
+                    '</tr>'
+            );
+            $('#' + $el.attr('id') + '-sum-row').autoNumeric('init', {aSep: '.', aDec: ',', aSign: ' €', pSign: 's'});
+
+            // update sum display
+            roundedsum = Math.round(sum);
+            $('.' + $el.attr('id') + '-sum').html(roundedsum + ' €');
+            if (roundedsum <= 0) {
+                $('.' + $el.attr('id') + '-sum').addClass('negative-sum');
+            }
+        }
+
+        this.add = function (transaction) {
+            collection.push(transaction);
+            paint();
+        };
+
+        this.update = function (transaction) {
+            collection[getTransactionItemIndexById(transaction.id)] = transaction;
+            paint();
+        };
+
+        this.remove = function (transaction) {
+            collection.splice(getTransactionItemIndexById(transaction.id), 1);
+            paint();
+        };
+
+        this.clear = function () {
+            collection = [];
+            paint();
+        };
+
+        this.getSum = function () {
+            return sum;
+        };
+    };
+
+    // APP ------------------------------------------
+    // Generic class for a Whyllet App
+    App = function (basename) {
+        this.basename = basename;
+
+        // initial load of all items from the store
+        var transactions = new Transactions($('#' + this.basename + '-transactions'));
+        hoodie.store.findAll(this.basename + 'item').then(function (items) {
+            items.forEach(transactions.add);
+        });
+        this.transactions = transactions;
+
+        // when an item changes, update the UI
+        hoodie.store.on(this.basename + 'item:add', this.transactions.add);
+        hoodie.store.on(this.basename + 'item:update', this.transactions.update);
+        hoodie.store.on(this.basename + 'item:remove', this.transactions.remove);
+        // clear items when user logs out
+        hoodie.account.on('signout', this.transactions.clear);
+
+        // on submit
+        $('#' + this.basename + '-panel').on('submit', function (event) {
+            event.preventDefault();
+
+            // fetch form data
+            var rawMemoVal = $('#' + basename + '-memo').val(),
+                inputDate = $('#' + basename + '-input-date'),
+                inputSubject = $('#' + basename + '-input-subject'),
+                inputAmount = $('#' + basename + '-input-amount'),
+                rawDate = inputDate.val(),
+                rawSubject = inputSubject.val(),
+                rawAmount = inputAmount.val(),
+                valDate,
+                strDate,
+                strSubject,
+                strAmount;
+
+            // save the "memo to myself"
+            if (rawMemoVal) {
+                // TODO save memo to myself
+                window.console.log('Memo: ' + rawMemoVal);
+            }
+
+            // create a new item
+            if (rawDate || rawSubject || rawAmount) {
+
+                // 1. validate date
+                valDate = moment(rawDate, ['DD.MM.YY', 'DD.MM.YYYY', 'MM/DD/YYYY']);
+                if (!valDate.isValid) {
+                    // TODO handle invalid date, error message & return without saving
+                    window.console.log('invalid date');
+                    return;
+                }
+                strDate = valDate.toDate().toISOString();
+
+                // 2. validate subject (escape HTML)
+                /*strSubject = escapeHtml(rawSubject);*/
+                // changed: don't escape anything here (but on output) to avoid escaping it twice
+                strSubject = rawSubject;
+
+                // 3. validate amount
+                strAmount = inputAmount.autoNumeric('get');
+                if (!strAmount || strAmount === 0) {
+                    // TODO handle empty amount field
+                    window.console.log('empty amount');
+                    return;
+                }
+                // make it a negative value
+                if (strAmount > 0) {
+                    strAmount *= -1;
+                }
+
+                // persist new item
+                hoodie.store.add(basename + 'item', {
+                    date: strDate,
+                    subject: strSubject,
+                    amount: strAmount
+                });
+                inputDate.val('');
+                inputSubject.val('');
+                inputAmount.val('');
+
+                // dbs = moment('7.06.2015', ['DD.MM.YY', 'DD.MM.YYYY', 'MM/DD/YYYY']).toDate().toISOString();
+                // whs = moment(new Date(dbs)).format('DD.MM.YYYY');
+            }
+        });
+    };
+
+    // MAIN FUNCTION --------------------------------
+    // execute when DOM is ready
+    $(function () {
+        var blinkHand,
+            spendApp;
+
+        // enable autoNumeric to help entering currency data
+        $('.autonumeric').autoNumeric('init', {aSep: '.', aDec: ',', aSign: ' €', pSign: 's'});
+
+        // insert today's date as default
+        $('.insertToday').val(moment().format('DD.MM.YYYY'));
+
+        // let the income hand blink
+        blinkHand = function () {
+            setTimeout(function () { $('#income-hand').toggleClass('text-success'); blinkHand(); }, 1000);
+        };
+        blinkHand();
+
+        // on submit of new income
+        $('#income-new-form').on('submit', function (event) {
+            event.preventDefault();
+            $('#income-dist-div').removeClass('hidden');
+        });
+
+        // close distribution panel on click on the upper right x
+        $('#income-dist-div-close').on('click', function (event) {
+            $('#income-dist-div').addClass('hidden');
+        });
+
+        // create apps
+        spendApp = new App('spend');
+    });
+}());
